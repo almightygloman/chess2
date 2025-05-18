6bd7f398bfbfafacf78205647a7c6cd8d7d464b3
almightygloman <zachivanovs@gmail.com>
2025-05-17 23:36:28 -0400

    "printboard function, removed test.c"

--- engine/main.c
+++ engine/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "defs.h"
 
 enum enumPiece
@@ -12,11 +13,34 @@ enum enumPiece
     nQueen,
     nKing
 };
-
 enum
 {
     TRUE,
     FALSE
+};
+enum
+{
+    FILE_A,
+    FILE_B,
+    FILE_C,
+    FILE_D,
+    FILE_E,
+    FILE_F,
+    FILE_G,
+    FILE_H,
+    FILE_NONE
+};
+enum
+{
+    RANK_1,
+    RANK_2,
+    RANK_3,
+    RANK_4,
+    RANK_5,
+    RANK_6,
+    RANK_7,
+    RANK_8,
+    RANK_NONE
 };
 
 struct
@@ -44,19 +68,42 @@ void init()
     BOARD.pieceBB[nQueen] = 0x0800000000000008;
     // All kings (e1, e8)
     BOARD.pieceBB[nKing] = 0x1000000000000010;
+
+    BOARD.populatedBB = BOARD.pieceBB[nWhite] | BOARD.pieceBB[nBlack];
+}
+
+void printboard()
+{
+    char c = '0';
+    for (int i = 63; i >= 0; i--)
+    {
+        U64 bit = 1ULL << i;
+        if (bit & BOARD.pieceBB[nPawn])
+            c = 'p';
+        if (bit & BOARD.pieceBB[nBishop])
+            c = 'b';
+        if (bit & BOARD.pieceBB[nKnight])
+            c = 'n';
+        if (bit & BOARD.pieceBB[nRook])
+            c = 'r';
+        if (bit & BOARD.pieceBB[nQueen])
+            c = 'q';
+        if (bit & BOARD.pieceBB[nKing])
+            c = 'k';
+
+        if ((bit & BOARD.populatedBB) == 0)
+            printf("%d", 0);
+        else
+            printf("%c", bit & BOARD.pieceBB[nWhite] ? toupper(c) : c);
+
+        if (i % 8 == 0)
+            printf("\n");
+    }
 }
 
 int main()
 {
     init();
-    U64 board = BOARD.pieceBB[nWhite] | BOARD.pieceBB[nBlack];
-    printf("%c", '\n');
-    for (int i = 0; i < 64; i++)
-    {
-        printf("%d", (1ULL << i) & board ? 1 : 0);
-        if ((i + 1) % 8 == 0 && i != 0)
-            printf("%c", '\n');
-    }
-    printf("%c", '\n');
+    printboard();
     return 0;
 }
